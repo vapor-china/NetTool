80cc5034e6f0aa97c635b1fd6a721caf935d691e
spectator.nan <zhuchunan001@sina.com>
2020-04-17 12:08:48 +0800

    Initial Commit

---
+++ NetTool/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  NetTool
+//
+//  Created by xj on 2020/4/17.
+//  Copyright © 2020 spectator.nan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
